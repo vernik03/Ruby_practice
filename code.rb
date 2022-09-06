11e3c2db6a6e38acb180125162cb0ef01d6dafe1
vernik03 <vernik03@gmail.com>
2022-09-07 02:10:06 +0300

    url

--- code.rb
+++ code.rb
@@ -1,5 +1,5 @@
 class Calc
-    @@wolfram = "https://www.wolframalpha.com/input?i=%28%28sin%283x%29%29%5E3+%2B+arctan%28f%29+-+6*%2810%5E3.1%29%29%2F%28%E2%88%9A%28a*x%5E2+%2B+b*x+%2Bc%29%29+%2B+%28e%5Ex%29*%28%7Ctan%28x%2B2%29%7C%29%2C+x+%3D+1%2C+a+%3D+2%2C+b%3D3%2C+c%3D4%2C+f%3D40"
+    @@wolfram = "http://surl.li/cxwwf"
     def initialize()
         @x = 1
         @a = 2
