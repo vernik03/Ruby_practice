44cdb80da8f72ebf93a65b9cd18b96bd1f0ec995
vernik03 <vernik03@gmail.com>
2022-09-09 09:28:23 +0300

    task 3

--- code.rb
+++ code.rb
@@ -1,46 +1,35 @@
 class Calc
     @@wolfram = "http://surl.li/cxwwf"
     def initialize()
-        @x = 1
-        @a = 2
-        @b = 3
-        @c = 4
-        @f = 40
+        @A = true
+        @B = false
+        @C = true
+        @X = 2
+        @Y = 10
+        @Z = -50
     end
 
     def count(default = false)
-        if !default
-            loop do 
-                readInput()
-                break if checkWrongInput()
-                puts "Input numbers again!"
-            end 
-        end
-        return countFunction()
+        readInput() if !default
+        return printResult()
     end
 
     def readInput()
-        print "Enter x: "
-        @x = gets.chomp.to_f
-        print "Enter a: "
-        @a = gets.chomp.to_f
-        print "Enter b: "
-        @b = gets.chomp.to_f
-        print "Enter c: "
-        @c = gets.chomp.to_f
-        print "Enter angle degree: "
-        @f = gets.chomp.to_f
+        print "Enter A: "
+        @A = gets.chomp.to_b
+        print "Enter B: "
+        @B = gets.chomp.to_b
+        print "Enter C: "
+        @C = gets.chomp.to_b
+        print "Enter X: "
+        @X = gets.chomp.to_f
+        print "Enter Y: "
+        @Y = gets.chomp.to_f
+        print "Enter Z: "
+        @Z = gets.chomp.to_f
     end
 
-    def checkWrongInput()
-        if (@a*(@x**2) + @b*@x + @c) <= 0
-            puts "Wrong input! a*x^2 + b*x + c must be > 0"
-            return false
-        end
-        return true
-    end
-
-    def countFunction()
+    def printResult()
         return ((Math.sin(3*@x)**3 + Math.atan(@f) - 6*10**3.1)/(Math.sqrt(@a*(@x**2) + @b*@x + @c)) + Math::E**@x*((Math.tan(@x+2)).abs))
     end
 end
