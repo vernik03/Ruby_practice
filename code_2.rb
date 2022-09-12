63485e9d360cde66c023bb5f810732f237273592
vernik03 <vernik03@gmail.com>
2022-09-12 18:45:44 +0300

    add task 2

---
+++ code_2.rb
@@ -0,0 +1,31 @@
+class Counter
+    def initialize()
+        @P = 2
+        @t = 16
+        @r = 8
+    end
+
+    def count(default = false)
+        readInput() if !default
+        return countResult()
+    end
+
+    def readInput()
+        print "Enter P: "
+        @P = gets.chomp.to_f
+        print "Enter t: "
+        @t = gets.chomp.to_f
+        print "Enter r: "
+        @r = gets.chomp.to_f
+    end
+
+    def countResult()
+        return @P ** @r * (1-@P ** (@t * -1)) 
+    end
+end
+
+
+counter = Counter.new
+puts "Range = #{counter.count(1)}" #default
+puts "Range = #{counter.count(1).to_f}" #default, float
+#puts "Range = #{polynom.square()}" #custom
