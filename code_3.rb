4ee2cba5b1fd417c3a0011ce8aa18bfb84e060f5
vernik03 <vernik03@gmail.com>
2022-09-12 20:24:25 +0300

    add task 2

---
+++ code_3.rb
@@ -0,0 +1,43 @@
+class Counter
+    def initialize()
+        @x = 2.0
+    end
+
+    def count1(default = false)
+        readInput() if !default
+        return countResult1()
+    end
+
+    def count2()
+        return countResult2()
+    end
+
+    def readInput()
+        print "Enter x: "
+        @x = gets.chomp.to_f
+    end
+
+    def countResult1()
+        res = 1
+        sign = -1
+        for i in 1..10
+            res += sign*((1.0+i/2.0+i)*@x**i)
+            sign *= -1
+        end
+        return res
+    end
+
+    def countResult2()
+        res = 1
+        for i in 1..8
+            res += 1/3.0**i
+        end
+        return res
+    end
+end
+
+
+counter = Counter.new
+puts "Sum = #{counter.count1(1)}" #default
+puts "Sum = #{counter.count2()}" #default
+#puts "Dec = #{counter.count()}" #custom
